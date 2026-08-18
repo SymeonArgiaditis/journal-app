7125c26ea0229d038422c8ec71102c4e67f88b78
Symeon Argiaditis <206509197+SymeonArgiaditis@users.noreply.github.com>
2026-08-18 12:33:38 +0300

    Refactor journal path handling in JournalWindow for clarity and consistency

--- journal_window.py
+++ journal_window.py
@@ -50,22 +50,18 @@ class JournalWindow(QMainWindow):
 
     def on_journal_clicked(self, item: QListWidgetItem):
         self.entry_list.clear()
-        # "Journals/[current_journal]": Local journal_path
-        journal_path = self.journals_path / item.text()
-        entries = list_entries(journal_path)
+        # "Journals/[current_journal]"
+        self.journal_path = self.journals_path / item.text()
+        entries = list_entries(self.journal_path)
 
         for entry in entries:
             self.entry_list.addItem(entry)
 
-        # "Journals/[current_journal]": Set current instance journal_path
-        self.current_journal_path = journal_path
-
     def on_entry_clicked(self, item: QListWidgetItem):
-        # "Journals/[current_journal]/[entry.md]":Local entry_path
-        entry_path = self.current_journal_path / item.text()
-
+        # "Journals/[current_journal]/[entry.md]"
+        self.entry_path = self.journal_path / item.text()
         # Read current entry and display in content menu
-        entry_text = entry_path.read_text()
+        entry_text = self.entry_path.read_text()
         # Text will be editable for now, implementing viewing and editing toggle in later iteration
         self.content.setMarkdown(entry_text)
 
